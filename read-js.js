357c4e6b1468d87a3a112c77989a45bc97fc365f
mao2009 <entrprise@gmail.com>
2019-12-27 10:32:56 +0900

    add read-js.js

---
+++ read-js.js
@@ -0,0 +1,11 @@
+function readJs(filename){
+    let text = ""
+    String.toString(fetch(filename).then(res => {return res.text()})).then(t => {text = t})
+    return text
+}
+
+function convertBookmarklet(js){
+    let bookmarklet = js.replace(/\/\*[\s\S]+?\*\//g,"").replace(/\/\/.+?\n/g,"")
+    bookmarklet = bookmarklet.replace(/\s+/g," ").replace("\n",";\n").replace(/;+?\n/g,";").replace("\n","");
+    return "javascript:" + bookmarklet
+}
